5b5b06c63977bb17bcd7e1e473499a3ee9eb96bd
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-06-21 09:29:44 +0100

    modified terraform details

--- providers.tf
+++ providers.tf
@@ -1,14 +1,18 @@
 #In this project, we will be using AWS as the provider, and this will tell Terraform the cloud provider we are going to create our resources in.
-/* terraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-} */
+}
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region            = "us-east-1"
+  access_key        = "AKIAUUPP6XST2VZRBAOW"
+  secret_access_key = "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"
+
 }
+
